4a90601edf2549421331b4301685e9e4302d0f9a
n-sleeman <141662174+n-sleeman@users.noreply.github.com>
2023-08-07 11:55:41 -0500

    Add files via upload

---
+++ Scrabble/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String [] args) {
+		Board b = new Board(true);
+//		ArrayList<String> dic = b.dictionary;
+//		int wordCount=0; 
+//		for(String word : dic) {
+//			System.out.println(word);
+//			wordCount++;
+//		}
+//		System.out.println(wordCount);
+	}
+	
+}
